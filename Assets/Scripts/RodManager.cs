fa325244dea4e1f599137726dac8ed47c7fa53cf
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-11-24 14:28:31 -0600

    Casting prototype

--- Assets/Scripts/RodManager.cs
+++ Assets/Scripts/RodManager.cs
@@ -23,7 +23,7 @@ public class RodManager : MonoBehaviour
 
     LineRenderer lineRenderer;
 
-    float power = 1;
+    float power = -3;
     bool casting = false;
 
     private void Start()
@@ -36,16 +36,25 @@ public class RodManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonUp(1))
+        // to cast
+        if (Input.GetMouseButton(1))
         {
             casting = true;
-            Instantiate(bobber, new Vector2(0, power / 10), Quaternion.identity);
         }
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (!GameObject.Find("PlaceHolderBobber(Clone)")) Instantiate(bobber, new Vector2(Random.Range(-7, 7), power), Quaternion.identity);
+            if (GameObject.Find("PlaceHolderBobber(Clone)")) Debug.Log("Already casted");
 
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, lineStartPoint.transform.position);
+            lineRenderer.SetPosition(1, GameObject.Find("PlaceHolderBobber(Clone)").transform.position);
+        }
         if (casting)
         {
-            if (power < 100) power += 1; // try to use delta time instead of a constant
-            if (power >= 100) { power = 100; casting = false; } // maybe run a `finish_cast` function or something
+            if (power < 4) power += Time.deltaTime; // try to use delta time instead of a constant
+            if (power >= 4) { power = 4; casting = false; } // maybe run a `finish_cast` function or something
         }
+        // end cast
     }
 }
